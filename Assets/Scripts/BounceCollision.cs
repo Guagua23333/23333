f400a28b978119995e1101e7c385c31e906fba06
xuekfk <love.kyngh.ky@gmail.com>
2026-06-19 06:45:47 +0800

    幹我好餓_主遊戲場景差最後調整了

---
+++ Assets/Scripts/BounceCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BounceCollision : MonoBehaviour
+{
+    public level 強度;
+}
+
+public enum level
+    {
+        Weak,
+        Normal,
+        Strong 
+    }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,142 @@
+using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxjumpForce = 100f;
+    public float minjumpForce = 10f;
+    public float maxChargeTime = 1f; //最常蓄力時間
+    
+    public float reboundForce = 50f;
+
+    private Rigidbody2D rb2D;
+    private Animator anim;
+    private SpriteRenderer _renderer; //圖片渲染顏色
+
+    private float buttonDowntime;
+    bool canJump = true;
+    
+    //取得滑鼠座標
+    private Vector2 mousePos;
+    float dirX;
+
+
+    private void Awake()
+    {
+        //初始化自動抓元件
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousepos();
+        LookAtMouse();
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            buttonDowntime = Time.time;
+        }
+
+        if (Input.GetMouseButtonUp(0)&& canJump)
+        {
+            Jump();
+        }
+    }
+
+    private void mousepos()
+    {
+        if (mousePos.x > transform.position.x)
+        {
+            dirX = 1f;   //右
+        }
+        else
+        {
+            dirX = -1f;  //左
+        }
+    }
+
+    private void LookAtMouse()
+    {
+        if (dirX > 0)
+        {
+            _renderer.flipX = false; // 面向右
+        }
+        else
+        {
+            _renderer.flipX = true;  // 面向左
+        }
+    }
+
+    private void Jump()
+    {
+        //計算按住時間
+        float holdTime = Time.time - buttonDowntime;
+
+        //限制最大蓄力時間
+        holdTime = Mathf.Clamp(holdTime, 0, maxChargeTime);
+
+        //換算力量
+        float force = Mathf.Lerp(
+            minjumpForce,
+            maxjumpForce,
+            holdTime / maxChargeTime
+        );
+        
+        rb2D.linearVelocity = Vector2.zero;
+        
+        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+        
+        //施加力量
+        rb2D.AddForce(direction * force, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;//可跳躍 "開啟"
+            
+        }
+
+        if (collision.gameObject.TryGetComponent(out BounceCollision bounce))
+        {
+            float finalForce = 0f;
+            
+            switch (bounce.強度)
+            {
+                case level.Weak:
+                    finalForce = reboundForce / 10f;
+                    break;
+                case level.Normal:
+                    finalForce = reboundForce / 5f;
+                    print("正常");
+                    break;
+                case level.Strong:
+                    finalForce = reboundForce;
+                    print("強反彈");
+                    break;
+            }
+           
+            Vector2 bounceDir = (transform.position - collision.transform.position).normalized;
+            bounceDir.y = -Mathf.Abs(bounceDir.y);
+
+            rb2D.AddForce(bounceDir * finalForce, ForceMode2D.Impulse);
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = false;//可跳躍 "關閉"
+        }
+    }
+}
